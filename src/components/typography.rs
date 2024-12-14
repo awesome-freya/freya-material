12c4809fa5156a956900948bc5c6c06f8136d9dd
Aiving <bellyinkognito05@gmail.com>
2024-12-14 12:08:05 +0500

    add support for more label attrs in typography

--- src/components/typography.rs
+++ src/components/typography.rs
@@ -6,6 +6,10 @@ pub fn Typography(
     #[props(default = TypescaleVariant::Body)] variant: TypescaleVariant,
     #[props(default = TypescaleSize::Medium)] size: TypescaleSize,
     #[props(default)] prominent: bool,
+    color: Option<String>,
+    text_align: Option<String>,
+    text_shadow: Option<String>,
+    max_lines: Option<String>,
     children: Element,
 ) -> Element {
     let (font_family, weight, size, _, line_height) = get_type_scale(variant, size, prominent);
@@ -16,6 +20,10 @@ pub fn Typography(
             font_weight: "{weight}",
             font_size: "{size}",
             line_height: "{line_height / size}",
+            color,
+            text_align,
+            text_shadow,
+            max_lines,
 
             {children}
         }
